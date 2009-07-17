192205dfe68f210ea9dcab0e2f4aa6dd8da8f18f
adrian <adrian@03946e4b-032c-0410-9851-fccc0fee27f7>
2009-07-17 13:15:11 +0000

    git-svn-id: https://svn.databasesandlife.com/data/trunk/dev/20081226-gwt-sudoku-solver@1652 03946e4b-032c-0410-9851-fccc0fee27f7

--- java-src/com/databasesandlife/sudoku/client/SudokuEntryPoint.java
+++ java-src/com/databasesandlife/sudoku/client/SudokuEntryPoint.java
@@ -1,28 +1,29 @@
-/*
- * SudokuEntryPoint.java
- *
- * Created on December 24, 2008, 6:42 PM
- *
- * To change this template, choose Tools | Template Manager
- * and open the template in the editor.
- */
-
 package com.databasesandlife.sudoku.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.ClickListener;
+import com.google.gwt.dom.client.AnchorElement;
+import com.google.gwt.dom.client.Document;
+import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.Grid;
-import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.InlineLabel;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Widget;
 
 /**
- *
  * @author Adrian Smith
  */
 public class SudokuEntryPoint implements EntryPoint {
+
+    public static class Link extends Widget {
+        public Link(String url, Widget stuff) {
+            AnchorElement e = Document.get().createAnchorElement();
+            setElement(e);
+            e.setHref(url);
+            e.appendChild(stuff.getElement());
+        }
+    }
 
     /** Creates a new instance of SudokuEntryPoint */
     public SudokuEntryPoint() {
@@ -33,31 +34,28 @@ public class SudokuEntryPoint implements EntryPoint {
         that declares an implementing class as an entry-point
     */
     public void onModuleLoad() {
-        final Label label = new Label("Hello, GWT!!!");
-        final Button button = new Button("Click me!");
-        
-        button.addClickListener(new ClickListener(){
-            public void onClick(Widget w) {
-                label.setVisible(!label.isVisible());
-            }
-        });
-        
-        RootPanel.get().add(button);
-        RootPanel.get().add(label);
-        
         Grid g = new Grid(9,9);
         g.setStylePrimaryName("sudoku");
         for (int y = 0; y < 9; y++) {
             if (y%3==0) g.getRowFormatter().setStylePrimaryName(y, "start");
             if (y==8)   g.getRowFormatter().setStylePrimaryName(y, "end");
             for (int x = 0; x < 9; x++) {
-                TextBox t = new TextBox();
-                t.setMaxLength(1);
-                g.setWidget(x, y, t);
-                if (x%3==0) g.getCellFormatter().setStylePrimaryName(y, x, "leftcol");
-                if (x==8)   g.getCellFormatter().setStylePrimaryName(y, x, "rightcol");
+                if (x == 1 && y == 1) {
+                    FlowPanel p = new FlowPanel();   //  <div> 
+                    p.add(new InlineLabel("text: "));
+                    p.add(new Image("http://www.uboot.com/status-image/?nick=adi1011"));
+                    Link link = new Link("http://www.uboot.com/", p);
+                    g.setWidget(y, x, link);
+                } else {
+                    TextBox t = new TextBox();
+                    t.setMaxLength(1);
+                    g.setWidget(y, x, t);
+                    if (x%3==0) g.getCellFormatter().setStylePrimaryName(y, x, "leftcol");
+                    if (x==8)   g.getCellFormatter().setStylePrimaryName(y, x, "rightcol");
+                }
             }
         }
         RootPanel.get().add(g);
+        GWT.
     }
 }
