07deb48616632f8454ef482e8dc7854eb47abb59
adrian <adrian@03946e4b-032c-0410-9851-fccc0fee27f7>
2010-05-15 13:42:13 +0000

    git-svn-id: https://svn.databasesandlife.com/data/trunk/dev/20081226-gwt-sudoku-solver@2115 03946e4b-032c-0410-9851-fccc0fee27f7

---
+++ 20081226-cooperative-algorithm/SudokuSolver.java
@@ -0,0 +1,25 @@
+package com.databasesandlife.sudoku.solver;
+
+import java.util.Arrays;
+
+/**
+ * @author Adrian Smith &lt;adrian.m.smith@gmail.com&gt;
+ */
+abstract public class SudokuSolver {
+
+    public static final class Result {
+        public enum Type {
+                                        ERR_TIMEOUT,
+                 /** No result found */ ERR_NONE,
+          /** Multiple results found */ WARN_MULTIPLE,
+                /** One result found */ UNIQUE
+        
+        };
+        public Type type;
+        public int[] board;
+        public Result(Type t)           { type=t; board=null; }
+        public Result(Type t, int[] b9) { type=t; board=SudokuSolverUtil.copy(b9); }
+    }
+    
+    abstract public Result solve(int board[]);
+}
---
+++ dist/README.txt
@@ -0,0 +1,7 @@
+This is the source to Databases & Life Sudoku Solver.
+
+The running version can be found here:
+
+  http://www.databasesandlife.com/sudoku
+  
+$Revision$
